2cf3ad740d8bc1a754bcd5c0b20bd8e65cbb3073
Daniel Reszka <dannn.r@gmail.com>
2019-08-26 14:14:59 +0200

    UI: setup apollo-client, fetch & render data

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+import { gql } from "apollo-boost"; // or you can use `import gql from 'graphql-tag';` instead
+
+function Books() {
+  const { loading, error, data } = useQuery(
+    gql`
+      {
+        books {
+          author
+          title
+        }
+      }
+    `
+  );
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return (
+    <ul>
+      {data.books.map(({ author, title }) => (
+        <li key={title}>
+          <p>
+            {title} <small> -- {author}</small>
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Books></Books>
     </div>
   );
 }
