3c76256d5e993cde7afe1a319e3c0572c31f32c6
Daniel Reszka <dannn.r@gmail.com>
2019-08-28 06:28:06 +0200

    UI: remove CRA unneeded files

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { useQuery } from "@apollo/react-hooks";
 
-import logo from "./logo.svg";
-import "./App.css";
-
 import { gql } from "apollo-boost"; // or you can use `import gql from 'graphql-tag';` instead
 
 function Books() {
