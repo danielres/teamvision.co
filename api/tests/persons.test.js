3352833fd84ac530ad596f85bd05894deda9484f
Daniel Reszka <dannn.r@gmail.com>
2019-09-01 20:26:14 +0200

    API: improve api test using deepEqual

--- api/tests/persons.test.js
+++ api/tests/persons.test.js
@@ -3,9 +3,9 @@ const execQuery = require("./support/execQuery");
 
 test("Query { persons {...} } works", async assert => {
   const data = await execQuery(`{ persons { email } }`);
-
   const expected = { persons: [] };
-  assert.equal(JSON.stringify(data), JSON.stringify(expected));
+
+  assert.deepEqual(data, expected);
 
   assert.end();
 });
