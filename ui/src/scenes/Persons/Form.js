c6e8c05af8d40e116b127339173312c01586771a
Daniel Reszka <dannn.r@gmail.com>
2019-09-02 21:36:12 +0200

    UI: render server-side validations on submit

--- ui/src/scenes/Persons/Form.js
+++ ui/src/scenes/Persons/Form.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMutation } from "@apollo/react-hooks";
+import flatten from "lodash/flatten";
+import get from "lodash/get";
+
 import { CREATE_PERSON, GET_PERSONS } from "./gql";
 
 export default function CreatePerson({ ButtonDone }) {
-  let nameInput;
-  let emailInput;
+  const initialValues = { email: "", name: "" };
+  const [values, setValues] = useState(initialValues);
+  const resetValues = () => setValues(initialValues);
 
   const [createPerson, response] = useMutation(CREATE_PERSON, {
     update: (cache, { data: { createPerson } }) => {
@@ -16,38 +20,39 @@ export default function CreatePerson({ ButtonDone }) {
     }
   });
 
+  const submit = e => {
+    e.preventDefault();
+    createPerson({ variables: values }).then(resetValues);
+  };
+
+  const errors = get(response, "error.graphQLErrors", []);
+  const validationErrors = flatten(
+    errors.map(e => get(e, "extensions.exception.errors", []))
+  );
+
   return (
     <div>
-      {response.error && (
-        <ul>
-          {response.error.graphQLErrors &&
-            response.error.graphQLErrors.map(e => <li>{e.message}</li>)}
+      {!!errors.length && (
+        <ul className="alert mb-4 p-4 list-inside">
+          {validationErrors.length
+            ? validationErrors.map(e => <li className="list-disc">{e}</li>)
+            : errors.map(e => <li>{e.message}</li>)}
         </ul>
       )}
 
-      <form
-        onSubmit={e => {
-          e.preventDefault();
-          createPerson({
-            variables: {
-              email: emailInput.value,
-              name: nameInput.value
-            }
-          });
-          emailInput.value = "";
-          nameInput.value = "";
-        }}
-      >
+      <form onSubmit={submit}>
         <input
           className="formInput mb-4"
           placeholder="Email"
-          ref={node => (emailInput = node)}
+          value={values.email}
+          onChange={e => setValues({ ...values, email: e.target.value })}
         />
 
         <input
           className="formInput mb-4"
           placeholder="Name"
-          ref={node => (nameInput = node)}
+          value={values.name}
+          onChange={e => setValues({ ...values, name: e.target.value })}
         />
 
         <div className="justify-between flex">
