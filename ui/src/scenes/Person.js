8eb60a64e358d3fd1dee8d8d74bd682b3c38e222
Daniel Reszka <dannn.r@gmail.com>
2019-09-05 14:03:49 +0200

    UI: add list of persons on Person page

--- ui/src/scenes/Person.js
+++ ui/src/scenes/Person.js
@@ -4,6 +4,7 @@ import React from "react";
 import { withRouter } from "react-router";
 
 import Avatar from "../components/Avatar";
+import PersonsTable from "./Persons/Table";
 
 const Person = ({ match, location, history }) => {
   const { id } = match.params;
@@ -23,14 +24,32 @@ const Person = ({ match, location, history }) => {
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
 
   return (
-    <section className="card">
-      <Avatar src={data.person.picture} />
-      {["name", "email"].map(k => (
+    <div className="flex">
+      <section className="w-64 card mr-4 hidden sm:block">
+        <PersonsTable />
+      </section>
+
+      <section className="w-full card flex">
         <div>
-          {k}: {data.person[k]}
+          <div className="w-24 h-24 md:w-48 md:h-48">
+            <Avatar size="full" src={data.person.picture} />
+          </div>
         </div>
-      ))}
-    </section>
+
+        <table className="spaced w-full ml-12">
+          <tbody>
+            <tr className="mb-6">
+              <th className="">Name</th>
+              <td className="">{data.person.name}</td>
+            </tr>
+            <tr className="mb-6">
+              <th className="">Email</th>
+              <td className="">{data.person.email}</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </div>
   );
 };
 
