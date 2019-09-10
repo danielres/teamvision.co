a789bf27b5af17ba6d0b3785766734f352e74e41
Daniel Reszka <dannn.r@gmail.com>
2019-09-10 11:28:13 +0200

    UI: add parsed query object to history location

--- ui/src/utils/history.js
+++ ui/src/utils/history.js
@@ -1,2 +1,28 @@
 import { createBrowserHistory } from "history";
-export default createBrowserHistory();
+
+const history = createBrowserHistory();
+
+const parseQueryString = query =>
+  query
+    ? (/^[?#]/.test(query) ? query.slice(1) : query)
+        .split("&")
+        .reduce((params, param) => {
+          let [key, value] = param.split("=");
+          params[key] = value
+            ? decodeURIComponent(value.replace(/\+/g, " "))
+            : "";
+          return params;
+        }, {})
+    : {};
+
+const addLocationQuery = history => {
+  history.location = {
+    ...history.location,
+    query: parseQueryString(history.location.search)
+  };
+};
+
+addLocationQuery(history);
+history.listen(() => addLocationQuery(history));
+
+export default history;
