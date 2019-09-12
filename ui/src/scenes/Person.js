4f1c35c6bf02f6f3abfe28c37125dedf52ae7411
Daniel Reszka <dannn.r@gmail.com>
2019-09-12 17:22:58 +0200

    UI: add fake skills+motivations to Person page

--- ui/src/scenes/Person.js
+++ ui/src/scenes/Person.js
@@ -1,11 +1,55 @@
 import { useQuery } from "@apollo/react-hooks";
 import { gql } from "apollo-boost"; // or you can use `import gql from 'graphql-tag';` instead
+import classnames from "classnames";
 import React from "react";
 import { withRouter } from "react-router";
-
 import Avatar from "../components/Avatar";
-import PersonsTable from "./Persons/Table";
 
+const skills = [
+  {
+    name: "React",
+    level: 5,
+    description:
+      "I have worked intensively with React during the last few years and gained extensive experience of React and its ecosystem."
+  },
+  {
+    name: "Neo4j",
+    level: 2,
+    description: ""
+  }
+];
+
+const motivations = [
+  {
+    name: "React",
+    level: 4,
+    description: ""
+  },
+  {
+    name: "Neo4j",
+    level: 5,
+    description: "I'd love to gain experience in Graph databases."
+  }
+];
+
+const TagsTable = ({ tags, colorClass = "" }) => (
+  <div className="table w-full">
+    {tags.map(t => (
+      <div key={t.name} className="table-row">
+        <div className="table-cell w-32">{t.name}</div>
+        <div className={classnames("table-cell w-16", colorClass)}>
+          {Array.from({ length: t.level })
+            .map(() => "▮")
+            .join("")
+            .padEnd(5, "▯")}
+        </div>
+        <div className="table-cell text-sm md:pl-12">
+          <div className="truncate w-64 text-gray-600">{t.description}</div>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 const Person = ({ match, location, history }) => {
   const { id } = match.params;
   const { loading, error, data } = useQuery(
@@ -24,11 +68,7 @@ const Person = ({ match, location, history }) => {
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
 
   return (
-    <div className="flex">
-      <section className="w-64 card mr-4 hidden sm:block">
-        <PersonsTable />
-      </section>
-
+    <div>
       <section className="w-full card flex">
         <div>
           <div className="w-24 h-24 md:w-48 md:h-48">
@@ -36,19 +76,31 @@ const Person = ({ match, location, history }) => {
           </div>
         </div>
 
-        <table className="spaced w-full ml-12">
-          <tbody>
-            <tr className="mb-6">
-              <th className="">Name</th>
-              <td className="">{data.person.name}</td>
-            </tr>
-            <tr className="mb-6">
-              <th className="">Email</th>
-              <td className="">{data.person.email}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="w-full ml-4 md:ml-8">
+          <table className="spaced">
+            <tbody>
+              <tr>
+                <th className="">Name</th>
+                <td className="">{data.person.name}</td>
+              </tr>
+              <tr>
+                <th className="">Email</th>
+                <td className="">{data.person.email}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
       </section>
+
+      <div className="card">
+        <h3 className="mb-4 text-lg">Motivations</h3>
+        <TagsTable tags={motivations} colorClass="text-pink-600" />
+      </div>
+
+      <div className="card">
+        <h3 className="mb-4 text-lg">Skills</h3>
+        <TagsTable tags={skills} colorClass="text-blue-600" />
+      </div>
     </div>
   );
 };
