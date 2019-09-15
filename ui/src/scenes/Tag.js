ad6d61dd726a658f8b4979084a1580497b5e9ad8
Daniel Reszka <dannn.r@gmail.com>
2019-09-16 00:36:25 +0200

    UI: Tag: add dummy buttons "Add to my skills/motivations"

--- ui/src/scenes/Tag.js
+++ ui/src/scenes/Tag.js
@@ -22,28 +22,45 @@ const Tag = ({ match, location, history }) => {
   if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
 
   return (
-    <div className="flex">
-      <section
-        className="card mr-4 hidden sm:block"
-        style={{ whiteSpace: "nowrap" }}
-      >
-        <Branch node={data.tag.name} />
-      </section>
+    <div>
+      <div className="mb-4 text-right mr-6 md:mr-0">
+        <button
+          title="Add to my motivations"
+          className="bg-white hover:text-white hover:bg-pink-500 btn-transparent mr-4"
+        >
+          <b>+</b> Motivations
+        </button>
+        <button
+          title="Add to my skills"
+          className="bg-white hover:text-white hover:bg-blue-500 btn-transparent"
+        >
+          <b>+</b> Skills
+        </button>
+      </div>
 
-      <section className="w-full card ">
-        <table className="spaced w-full">
-          <tbody>
-            <tr className="mb-6">
-              <th className="">Name</th>
-              <td className="">{data.tag.name}</td>
-            </tr>
-            <tr className="mb-6">
-              <th className="">Description</th>
-              <td className="">{data.tag.description}</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
+      <div className="flex">
+        <section
+          className="card mr-4 hidden sm:block"
+          style={{ whiteSpace: "nowrap" }}
+        >
+          <Branch node={data.tag.name} />
+        </section>
+
+        <section className="w-full card ">
+          <table className="spaced w-full">
+            <tbody>
+              <tr className="mb-6">
+                <th className="">Name</th>
+                <td className="">{data.tag.name}</td>
+              </tr>
+              <tr className="mb-6">
+                <th className="">Description</th>
+                <td className="">{data.tag.description}</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+      </div>
     </div>
   );
 };
