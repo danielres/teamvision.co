fa370d41e7e5500eaeef0641aafa1f7d13763ae1
Daniel Reszka <dannn.r@gmail.com>
2019-09-16 20:13:12 +0200

    API: neo4jDriver: disable lossless integers

--- api/src/neo4jDriver.js
+++ api/src/neo4jDriver.js
@@ -4,7 +4,8 @@ const neo4j = require("neo4j-driver").v1;
 
 const driver = neo4j.driver(
   env.NEO4J_URL,
-  neo4j.auth.basic(env.NEO4J_USER, env.NEO4J_PASSWORD)
+  neo4j.auth.basic(env.NEO4J_USER, env.NEO4J_PASSWORD),
+  { disableLosslessIntegers: true }
 );
 
 module.exports = driver;
