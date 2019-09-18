d387f020da8493056ed40dc57e2e8ef539c92f34
Daniel Reszka <dannn.r@gmail.com>
2019-09-18 18:16:15 +0200

    UI: Profile: improve styling

--- ui/src/scenes/Profile.js
+++ ui/src/scenes/Profile.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { gql } from "apollo-boost"; // or you can use `import gql from 'graphql-tag';` instead
 import { useQuery } from "@apollo/react-hooks";
-
+import truncate from "lodash/truncate";
 import Avatar from "../components/Avatar";
+import { upperFirst } from "../utils/strings";
 
 export default function() {
   const { loading, error, data } = useQuery(
@@ -27,17 +28,32 @@ export default function() {
 
   return (
     <section className="card">
-      <Avatar src={data.userInfo.picture} />
+      <div className="flex">
+        <div className="w-64 mr-6 pr-6">
+          <Avatar src={data.userInfo.picture} />
+        </div>
 
-      <div className="bg-gray-100 my-4 p-4">
-        {Object.entries(data.userInfo)
-
-          .filter(([k, v]) => !k.startsWith("__"))
-          .map(([k, v]) => (
-            <div>
-              {k}: {`${v}`}
-            </div>
-          ))}
+        <div className="mb-4 w-full">
+          <h2 className="text-lg ">Authentication data (Private)</h2>
+          <div className="bg-gray-100 p-4 border rounded text-gray-700 ">
+            {Object.entries(data.userInfo)
+              .filter(([k, v]) => !k.startsWith("__"))
+              .map(([k, v]) => (
+                <div className="flex mb-1" key={k}>
+                  <div className="w-64">{upperFirst(k.replace("_", " "))}</div>
+                  <div>
+                    {String(v).startsWith("http") ? (
+                      <a href={v} title={v} target="__blank">
+                        {truncate(v, { length: 60, separator: "&hellip;" })}
+                      </a>
+                    ) : (
+                      truncate(v, { length: 60, separator: "&hellip;" })
+                    )}
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
       </div>
     </section>
   );
