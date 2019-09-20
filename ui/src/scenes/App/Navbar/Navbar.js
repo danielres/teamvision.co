97e9a689070b70d976ba9e67a742022720c9d575
Daniel Reszka <dannn.r@gmail.com>
2019-09-20 12:18:51 +0200

    UI: Navbar: reduce height, improve styling

--- ui/src/scenes/App/Navbar/Navbar.js
+++ ui/src/scenes/App/Navbar/Navbar.js
@@ -6,7 +6,8 @@ import { useAuth } from "../../../providers/auth";
 import IconBurger2 from "./IconBurger";
 
 const c = {
-  link: "text-sm block md:inline-block md:mt-0 hover:text-white mr-4",
+  link:
+    "text-sm block md:inline-block md:mt-0 hover:text-white mr-4 font-semibold ",
   link_active: "text-white",
   button:
     "px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white leading-none text-sm"
@@ -22,10 +23,10 @@ export default () => {
   const onBurgerClick = () => setIsBurgerNavOpen(!isBurgerNavOpen);
 
   return (
-    <nav className="flex items-baseline justify-between flex-wrap bg-teal-500 p-6 shadow-md">
+    <nav className="flex items-baseline justify-between flex-wrap bg-teal-500 px-6 py-2 shadow-md">
       <div className="flex flex-shrink-0 text-teal-200 mr-6">
         {/* TODO: insert svg logo here */}
-        <Link to="/" className="font-semibold text-lg">
+        <Link to="/" className={classnames(c.link, "text-lg")}>
           Teamvision
         </Link>
       </div>
