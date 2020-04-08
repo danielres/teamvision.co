93eb748b0fe69d342f061e1111521f2c5ff99a1d
Daniel Reszka <dannn.r@gmail.com>
2020-04-08 14:06:58 +0200

    API: hash password + validate args on User.insert()

--- api/store/factories/User.js
+++ api/store/factories/User.js
@@ -1,14 +1,21 @@
+import { hashPassword } from '../../src/utils';
+import userValidator from '../validators/userValidator';
+
 export default knex => tenantId => {
   const queries = {};
 
   queries.all = () => knex('User').where('tenantId', tenantId);
 
-  queries.insert = async args =>
-    (
+  queries.insert = async ({ password, ...args }) => {
+    await userValidator.insert.validate({ password, ...args }, { abortEarly: false });
+    const hash = await hashPassword(password);
+
+    return (
       await knex('User')
-        .insert({ tenantId, ...args })
+        .insert({ tenantId, password: hash, ...args })
         .returning('*')
     )[0];
+  };
 
   return queries;
 };
---
+++ api/store/validators/userValidator.js
@@ -0,0 +1,18 @@
+import * as yup from 'yup';
+
+export default {
+  insert: yup.object().shape({
+    name: yup
+      .string()
+      .min(3)
+      .required(),
+    email: yup
+      .string()
+      .email()
+      .required(),
+    password: yup
+      .string()
+      .min(6)
+      .required(),
+  }),
+};
