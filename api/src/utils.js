76434371cf46cb7da3a0e63fb09e754e1cb02cda
Daniel Reszka <dannn.r@gmail.com>
2020-04-08 14:04:42 +0200

    API: add password hashing utils

--- api/src/utils.js
+++ api/src/utils.js
@@ -1,5 +1,16 @@
+import bcrypt from 'bcrypt';
+import config from '../config';
+
+const {
+  bcrypt: { saltRounds },
+} = config;
+
 export const allowOnlyInEnvs = (...envs) => {
   if (!envs.includes(process.env.NODE_ENV)) throw new Error('Forbidden action in this environment');
 };
+
+export const hashPassword = password => bcrypt.hash(password, saltRounds);
+
+export const verifyPassword = bcrypt.compare;
 
 export const sleep = duration => new Promise(resolve => setTimeout(resolve, duration));
