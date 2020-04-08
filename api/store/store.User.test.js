5f5385fe985cb7dae86e4bb13cb0ecfc7f022382
Daniel Reszka <dannn.r@gmail.com>
2020-04-08 14:08:48 +0200

    API: update & fix User store tests

--- api/store/store.User.test.js
+++ api/store/store.User.test.js
@@ -1,5 +1,7 @@
-import { NotNullViolationError, UniqueViolationError } from 'db-errors';
+import { UniqueViolationError } from 'db-errors';
 import { sortBy } from 'lodash/fp';
+import { ValidationError } from 'yup';
+import { verifyPassword } from '../src/utils';
 import store from './store';
 import { samples } from './test/support';
 
@@ -18,12 +20,8 @@ describe(`User(tenantId)`, () => {
     it(`returns all by "tenantId"`, async done => {
       const { id: tenantId1 } = await Tenant.insert(tenant1);
       const { id: tenantId2 } = await Tenant.insert(tenant2);
-
       const User1 = store.User(tenantId1);
-      await User1.insert(user1_1);
-
       const User2 = store.User(tenantId2);
-
       await User2.insert(user2_1);
       await User2.insert(user2_2);
 
@@ -42,45 +40,45 @@ describe(`User(tenantId)`, () => {
       const User = store.User(tenantId);
 
       const actual = await User.insert(anne);
-      expect(actual).toHaveProperty('id');
-      expect(actual).toHaveProperty('createdAt');
-      expect(actual).toHaveProperty('updatedAt');
-
+      expect(Object.keys(actual)).toEqual(['id', 'tenantId', 'email', 'name', 'password', 'createdAt', 'updatedAt']);
       expect(actual.name).toEqual(anne.name);
       expect(actual.tenantId).toEqual(tenantId);
       done();
     });
 
+    describe('password', () => {
+      it('is hashed', async done => {
+        const { id: tenantId } = await Tenant.insert(tenant1);
+        const User = store.User(tenantId);
+
+        await User.insert(anne);
+        const dbAnne = (await User.all())[0];
+        const isCorrectPassword = await verifyPassword(anne.password, dbAnne.password);
+        expect(isCorrectPassword).toBe(true);
+        done();
+      });
+    });
+
     describe(`validations`, () => {
-      it(`requires "name"`, async () => {
+      it(`requires email + name + password`, async () => {
         const { id: tenantId } = await Tenant.insert(tenant1);
         const User = store.User(tenantId);
 
-        return User.insert({ ...anne, name: undefined }).catch(e => {
-          expect(e.column).toEqual('name');
-          expect(e instanceof NotNullViolationError).toEqual(true);
+        return User.insert({}).catch(e => {
+          expect(e instanceof ValidationError).toEqual(true);
+          expect(e.errors).toEqual([
+            'name is a required field',
+            'email is a required field',
+            'password is a required field',
+          ]);
         });
       });
 
-      it(`requires "email"`, async () => {
-        const { id: tenantId } = await Tenant.insert(tenant1);
-        const User = store.User(tenantId);
-
-        return User.insert({ ...anne, email: undefined }).catch(e => {
-          expect(e.column).toEqual('email');
-          expect(e instanceof NotNullViolationError).toEqual(true);
-        });
-      });
-
-      it(`requires ["name", "tenantId"] to be unique`, async () => {
+      it(`requires [name, tenantId] to be unique`, async () => {
         const { id: tenantId1 } = await Tenant.insert(tenant1);
         const { id: tenantId2 } = await Tenant.insert(tenant2);
-
         const User1 = store.User(tenantId1);
         const User2 = store.User(tenantId2);
-
-        await User1.insert({ ...user1_1, name: 'same' });
-        await User2.insert({ ...user1_1, name: 'same' });
 
         return User1.insert({ ...user1_1, name: 'same' }).catch(e => {
           expect(e.columns).toEqual(['name', 'tenantId']);
@@ -89,20 +87,51 @@ describe(`User(tenantId)`, () => {
         });
       });
 
-      it(`requires ["email", "tenantId"] to be unique`, async () => {
-        const { id: tenantId1 } = await Tenant.insert(tenant1);
-        const { id: tenantId2 } = await Tenant.insert(tenant2);
+      it(`requires [email, tenantId] to be unique`, async () => {
+        const { id: tenantId } = await Tenant.insert(tenant1);
+        const User = store.User(tenantId);
 
-        const User1 = store.User(tenantId1);
-        const User2 = store.User(tenantId2);
-
-        await User1.insert({ ...user1_1, email: 'same@example.com' });
-        await User2.insert({ ...user1_1, email: 'same@example.com' });
-
-        return User1.insert({ ...user2_1, email: 'same@example.com' }).catch(e => {
+        await User.insert({ ...user1_1, email: 'same@example.com' });
+        return User.insert({ ...user2_1, email: 'same@example.com' }).catch(e => {
           expect(e.columns).toEqual(['email', 'tenantId']);
           expect(e.constraint).toEqual('user_email_tenantid_unique');
           expect(e instanceof UniqueViolationError).toEqual(true);
+        });
+      });
+
+      describe('email', () => {
+        it('must be a valid email', async () => {
+          const { id: tenantId } = await Tenant.insert(tenant1);
+          const User = store.User(tenantId);
+
+          return User.insert({ ...anne, email: 'invalidEmail' }).catch(e => {
+            expect(e instanceof ValidationError).toEqual(true);
+            expect(e.errors).toEqual(['email must be a valid email']);
+          });
+        });
+      });
+
+      describe('name', () => {
+        it('must be at least 3 chars', async () => {
+          const { id: tenantId } = await Tenant.insert(tenant1);
+          const User = store.User(tenantId);
+
+          return User.insert({ ...anne, name: 'ab' }).catch(e => {
+            expect(e instanceof ValidationError).toEqual(true);
+            expect(e.errors).toEqual(['name must be at least 3 characters']);
+          });
+        });
+      });
+
+      describe('password', () => {
+        it('must be at least 6 chars', async () => {
+          const { id: tenantId } = await Tenant.insert(tenant1);
+          const User = store.User(tenantId);
+
+          return User.insert({ ...anne, password: '12345' }).catch(e => {
+            expect(e instanceof ValidationError).toEqual(true);
+            expect(e.errors).toEqual(['password must be at least 6 characters']);
+          });
         });
       });
     });
--- api/store/test/support.js
+++ api/store/test/support.js
@@ -11,11 +11,11 @@ export const samples = {
     topic2_2: { name: 'topic2-2' },
   },
   users: {
-    anne: { name: 'Anne', email: 'anne@example.com' },
-    john: { name: 'John', email: 'john@example.com' },
-    user1_1: { name: 'user1-1', email: 'user1-1@example.com' },
-    user1_2: { name: 'user1-2', email: 'user1-2@example.com' },
-    user2_1: { name: 'user2-1', email: 'user2-1@example.com' },
-    user2_2: { name: 'user2-2', email: 'user2-2@example.com' },
+    anne: { name: 'Anne', email: 'anne@example.com', password: '123456' },
+    john: { name: 'John', email: 'john@example.com', password: '123456' },
+    user1_1: { name: 'user1-1', email: 'user1-1@example.com', password: '123456' },
+    user1_2: { name: 'user1-2', email: 'user1-2@example.com', password: '123456' },
+    user2_1: { name: 'user2-1', email: 'user2-1@example.com', password: '123456' },
+    user2_2: { name: 'user2-2', email: 'user2-2@example.com', password: '123456' },
   },
 };
