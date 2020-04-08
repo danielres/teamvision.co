64dd8d4e563e589fececf052e2ee4dd22dcab8bc
Daniel Reszka <dannn.r@gmail.com>
2020-04-08 14:03:51 +0200

    API: add dummy email sender with signUpSuccess handler

---
+++ api/emails/sender.js
@@ -0,0 +1,17 @@
+export default {
+  signUpSuccess: ({ name, email }) => {
+    console.log('');
+    console.log(`== SEND EMAIL to ${email} `.padEnd(70, '='));
+    console.log(`
+      Hi ${name}!
+
+      Thank you for signing up.
+      Your account has been created.
+
+      Please click on the following link to verify your email:
+      [TODO].
+    `);
+    console.log(''.padEnd(70, '='));
+    console.log('');
+  },
+};
