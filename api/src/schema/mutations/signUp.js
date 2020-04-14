17c57095aec63245f4388cae894b514fc7c138c4
Daniel Reszka <dannn.r@gmail.com>
2020-04-14 21:36:18 +0200

    API: signUp: add shortId + add testSpy, solve failing test

--- api/src/schema/mutations/signUp.js
+++ api/src/schema/mutations/signUp.js
@@ -1,16 +1,19 @@
+import shortid from 'shortid';
 import { ValidationError } from 'yup';
 import sender from '../../../emails/sender';
 import store from '../../../store/store';
+import testSpy from '../../../test/testSpy';
 import { reportError } from '../../utils';
 import ServerError from '../errors/ServerError';
 
 export default async (parent, { args }) => {
   try {
     const { Tenant } = store;
-    const tenant = await Tenant.insert({ name: `Tenant-${Math.random()}` });
+    const tenant = await Tenant.insert({ name: `Tenant-${Math.random()}`, shortId: shortid.generate() });
     const User = store.User(tenant.id);
     const { email, name } = await User.insert(args);
     await sender.signUpSuccess({ email, name });
+    testSpy.spy({ SignUp: { tenant } });
     return true;
   } catch (error) {
     if (error instanceof ValidationError) return error;
