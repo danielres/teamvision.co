8d980ed56d271dd43c647c906f253ebc301d0c72
Daniel Reszka <dannn.r@gmail.com>
2020-04-18 13:49:37 +0200

    UI: css: buttons: cleanup using classnames

--- ui/src/css/buttons.js
+++ ui/src/css/buttons.js
@@ -1,15 +1,21 @@
-/* eslint-disable prefer-template */
-const base = `inline-block `;
-const link = `text-gray-600 rounded hover:underline `;
-const spacing = `py-2 px-4 `;
+import classnames from 'classnames';
+
+const base = `inline-block`;
+const link = `text-gray-600 rounded hover:underline`;
+const spacing = `py-2 px-4`;
 
 const colored = color =>
-  base + spacing + `bg-${color}-500 hover:bg-${color}-700 text-white font-bold rounded outline-none`;
+  classnames(
+    base,
+    spacing,
+    `bg-${color}-500 hover:bg-${color}-700 text-white font-bold rounded outline-none`,
+    //
+  );
 
 export default {
-  dev: link + 'font-bold text-red-700 outline-none',
+  dev: classnames(link, 'font-bold text-red-700 outline-none'),
   gray: colored('gray'),
   primary: colored('blue'),
-  transparent: base + spacing + link,
+  transparent: classnames(base, spacing, link),
   text: link,
 };
