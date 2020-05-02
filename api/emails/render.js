8e182af4a2d1c29cf5812482a59d1021e5343fd6
Daniel Reszka <dannn.r@gmail.com>
2020-05-02 14:53:29 +0200

    API: add NODE_ENV to console output on email sent

--- api/emails/render.js
+++ api/emails/render.js
@@ -2,7 +2,7 @@
 
 export default ({ content, email }) => {
   console.log('');
-  console.log(`== SEND EMAIL to ${email} `.padEnd(70, '='));
+  console.log(`== SEND EMAIL to ${email} == (${process.env.NODE_ENV})`.padEnd(70, '='));
   console.log(content);
   console.log(''.padEnd(70, '='));
   console.log('');
