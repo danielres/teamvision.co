2b6e5c645db658dcc9a581d50f301bdd51b41464
Daniel Reszka <dannn.r@gmail.com>
2020-05-06 11:10:33 +0200

    SQ API: signin allow for email verified only

--- api/src/errors/SignInError.js
+++ api/src/errors/SignInError.js
@@ -2,7 +2,8 @@ import testSpy from '../../test/testSpy';
 import SafeError from './SafeError';
 
 export default class SignInError extends SafeError {
-  constructor({ message = 'Invalid email or password', internalData = 'AAA' } = {}) {
+  // FIXME
+  constructor({ message = 'Invalid email or password', internalData } = {}) {
     super();
     testSpy.spy({ SignInError: { internalData } });
     this.message = message;
