f864931c7e58fee05b48fddc09273e4cd261f428
Daniel Reszka <dannn.r@gmail.com>
2020-05-06 12:32:51 +0200

    UI: EnvHeader: add link to mailcatcher

--- ui/config.js
+++ ui/config.js
@@ -1,5 +1,6 @@
 import { number, object, string } from 'yup';
 
+const isDev = process.env.NODE_ENV === 'development';
 const isTest = process.env.NODE_ENV === 'test';
 
 const config = {
@@ -10,6 +11,12 @@ const config = {
   devServer: {
     port: isTest ? process.env.UI_DEV_SERVER_PORT_TEST : process.env.UI_DEV_SERVER_PORT,
   },
+
+  mailcatcher: {
+    ui: {
+      url: process.env.MAILCATCHER_UI_URL,
+    },
+  },
 };
 
 const validator = object().shape({
@@ -19,6 +26,12 @@ const validator = object().shape({
 
   devServer: object().shape({
     port: number().required(),
+  }),
+
+  mailcatcher: object().shape({
+    ui: object().shape({
+      url: isDev || isTest ? string().required() : string(),
+    }),
   }),
 });
 
--- ui/src/components/EnvHeader.jsx
+++ ui/src/components/EnvHeader.jsx
@@ -1,37 +1,47 @@
 import React from 'react';
 import config from '../../config';
 
-const classes = (tint = 'teal') => `p-1 text-sm text-${tint}-200 bg-${tint}-600 flex justify-between`;
+const API_URL = config.api.uri;
+const MAILCATCHER_UI_URL = config.mailcatcher.ui.url;
 
 const css = {
-  test: classes('teal'),
-  staging: classes('green'),
+  outer: tint => `py-1 px-8 text-sm text-${tint}-200 bg-${tint}-600 flex justify-between`,
+  right: {
+    outer: `flex`,
+  },
+  separator: `px-2`,
+};
+
+const { NODE_ENV } = process.env;
+
+const EnvHeader = ({ className }) => {
+  return (
+    <div className={className}>
+      <div>{NODE_ENV.toUpperCase()}</div>
+
+      <div className={css.right.outer}>
+        <div>
+          <a href={API_URL} rel="noopener noreferrer" target="_blank">
+            api
+          </a>
+        </div>
+
+        <div className={css.separator} />
+
+        <div>
+          <a href={MAILCATCHER_UI_URL} rel="noopener noreferrer" target="_blank">
+            mailcatcher
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default () => {
-  if (process.env.NODE_ENV === 'test')
-    return (
-      <div className={css.test}>
-        <div>
-          <b>TEST</b>
-        </div>
-        <div>
-          api: <b>{config.api.uri}</b>
-        </div>
-      </div>
-    );
-
-  if (process.env.NODE_ENV === 'staging')
-    return (
-      <div className={css.staging}>
-        <div>
-          <b>STAGING</b>
-        </div>
-        <div>
-          api: <b>{config.api.uri}</b>
-        </div>
-      </div>
-    );
+  if (NODE_ENV === 'development') return <EnvHeader className={css.outer('gray')} />;
+  if (NODE_ENV === 'staging') return <EnvHeader className={css.outer('green')} />;
+  if (NODE_ENV === 'test') return <EnvHeader className={css.outer('test')} />;
 
   return null;
 };
